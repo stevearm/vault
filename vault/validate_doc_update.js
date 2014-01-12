73f3824ee8a52e1b95d6c072073c1b403fb0ad32
Steve Armstrong <steve@horsefire.com>
2014-01-12 14:22:00 -0800

    Must allow a single entry in vault db

--- vault/validate_doc_update.js
+++ vault/validate_doc_update.js
@@ -1,3 +1,9 @@
 function(newDoc, oldDoc, userCtx) {
-	throw ({"forbidden":"no db entries belong in the vault ui database"});
+	if (newDoc._id == "id") {
+		if (!("vaultId" in newDoc)) {
+			throw ({"forbidden":"id document must specify a vaultId"});
+		}
+		return;
+	}
+	throw ({"forbidden":"only a single db entry (_id == 'id') belongs in the vault database"});
 }
