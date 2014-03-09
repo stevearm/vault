3e8e252654569b07191f618c92f1a6b9304a7642
Steve Armstrong <steve@horsefire.com>
2014-03-09 01:33:53 -0800

    Adding repair flow for each of the issues on the report card

--- vault/_attachments/js/install.js
+++ vault/_attachments/js/install.js
@@ -3,10 +3,85 @@
 angular.module("vault", [ "vault.services.installer", "CornerCouch" ])
 
 .controller("InstallCtrl", [
-    "$scope", "$http", "cornercouch", "InstallService",
-    function($scope, $http, cornercouch, InstallService) {
-        InstallService.getReportCard(function(report) {
-            $scope.report = report;
-        });
+    "$scope", "$http", "cornercouch", "InstallService", "CouchAppService",
+    function($scope, $http, cornercouch, InstallService, CouchAppService) {
+
+        var couchServer = cornercouch();
+
+        $scope.refresh = function() {
+            $scope.report = null;
+            setFix();
+            InstallService.getReportCard(function(report) {
+                $scope.report = report;
+                if (!report.pass) {
+                    for (var i = 0; i < report.parts.length; i++) {
+                        if (!report.parts[i].pass) {
+                            setFix(report.parts[i]);
+                            break;
+                        }
+                    }
+                }
+            });
+        };
+
+        var setFix = function(part) {
+            if (typeof(part) === "undefined") {
+                $scope.fix = null;
+                return;
+            }
+            $scope.fix = {
+                key: part.key,
+                run: function() { alert("Not yet implemented"); }
+            };
+
+            switch($scope.fix.key) {
+                case "idDoc":
+                    $scope.fix.vaultDbName = part.baggage.vaultDbName;
+                    if ("vaultId" in part.baggage) {
+                        $scope.fix.vaultId = part.baggage.vaultId;
+                    } else {
+                        $http.get("/").success(function(data) {
+                            if (data.uuid) {
+                                $scope.fix.vaultId = data.uuid;
+                            } else {
+                                $http.get("/_uuids").success(function(data) {
+                                    $scope.fix.vaultId = data.uuids[0];
+                                });
+                            }
+                        });
+                    }
+                    $http.get("/_all_dbs").success(function(data) {
+                        $scope.fix.dbNames = data.filter(function(e) {
+                            return e.charAt(0) != "_";
+                        });
+                    });
+                    $scope.fix.run = function() {
+                        InstallService.fixIdDoc($scope.fix.vaultId, $scope.fix.vaultDbName, function() {
+                            $scope.refresh();
+                        });
+                    };
+                    break;
+                case "vaultDbSec":
+                    $scope.fix.run = function() {
+                        InstallService.fixDbMembers(part.baggage.vaultDbName, function() {
+                            $scope.refresh();
+                        });
+                    };
+                    break;
+                case "vaultDbVault":
+                    $scope.fix.signature = null;
+                    $http.get("/").success(function(data) {
+                        $scope.fix.signature = data;
+                    });
+                    $scope.fix.run = function() {
+                        InstallService.fixVaultDbEntry(part.baggage.vaultDbName, part.baggage.vaultId, $scope.fix.signature, function() {
+                            $scope.refresh();
+                        });
+                    };
+                    break;
+            }
+        };
+
+        $scope.refresh();
     }
 ]);
--- vault/_attachments/js/installService.js
+++ vault/_attachments/js/installService.js
@@ -5,22 +5,23 @@ angular.module("vault.services.installer", ["couchapp.service", "CornerCouch"])
 .service("InstallService", [
     "$http", "$location", "CouchAppService", "cornercouch",
     function($http, $location, CouchAppService, cornercouch) {
-        var createResponse = function(m) {
-            var createPart = function(key, title, message) {
+        var createResponse = function(m, b) {
+            var createPart = function(key, title, message, baggage) {
                 if (typeof(message) === "undefined") { message = "Unknown result"; }
                 var result = { key: key, title: title, pass: true };
                 if (message != null) {
                     result.pass = false;
                     result.message = message;
                 }
+                result.baggage = baggage || {};
                 return result;
             }
 
             var parts = [
-                createPart("serverSec",     "Security enabled on server",               m.serverSec),
-                createPart("idDoc",         "Current db has valid id document",         m.idDoc),
-                createPart("vaultDbSec",    "VaultDb is only viewable by admins",       m.vaultDbSec),
-                createPart("vaultDbVault",  "VaultDb has document for current vault",   m.vaultDbVault)
+                createPart("serverSec",     "Security enabled on server",               m.serverSec,    b.serverSec),
+                createPart("idDoc",         "Current db has valid id document",         m.idDoc,        b.idDoc),
+                createPart("vaultDbSec",    "VaultDb is only viewable by admins",       m.vaultDbSec,   b.vaultDbSec),
+                createPart("vaultDbVault",  "VaultDb has document for current vault",   m.vaultDbVault, b.vaultDbVault)
             ];
 
             return {
@@ -34,10 +35,11 @@ angular.module("vault.services.installer", ["couchapp.service", "CornerCouch"])
         this.getReportCard = function(callback) {
 
             var messages = { serverSec: "Untested", idDoc: "Untested", vaultDbSec: "Untested", vaultDbVault: "Untested" };
+            var baggage = {};
 
             $http.get("/_session").error(function(data, status) {
                 messages.serverSec = "Error getting current session information";
-                callback(createResponse(messages));
+                callback(createResponse(messages, baggage));
                 return;
             }).success(function(data) {
                 if (data.userCtx.name == null && data.userCtx.roles.indexOf("_admin") != -1) {
@@ -52,47 +54,50 @@ angular.module("vault.services.installer", ["couchapp.service", "CornerCouch"])
                 var idDoc = new currentDb.docClass();
                 idDoc.load("id").error(function() {
                     messages.idDoc = "Error loading doc";
-                    callback(createResponse(messages));
+                    callback(createResponse(messages, baggage));
                     return;
                 }).success(function() {
+                    baggage.idDoc = idDoc;
                     if (!("vaultId" in idDoc)) {
                         messages.idDoc = "'vaultId' not specified";
-                        callback(createResponse(messages));
+                        callback(createResponse(messages, baggage));
                         return;
                     }
                     if (!("vaultDbName" in idDoc) || idDoc.vaultDbName == "") {
                         messages.idDoc = "'vaultDbName' not specified";
-                        callback(createResponse(messages));
+                        callback(createResponse(messages, baggage));
                         return;
                     }
                     messages.idDoc = null;
 
+                    baggage.vaultDbSec = { vaultDbName: idDoc.vaultDbName };
                     $http.get("/" + idDoc.vaultDbName + "/_security").error(function(data, status) {
                         if (status == 404) {
                             messages.idDoc = "'vaultDbName' " + idDoc.vaultDbName + " does not exist";
-                            callback(createResponse(messages));
+                            callback(createResponse(messages, baggage));
                             return;
                         }
                         messages.vaultDbSec = "Cannot read security info (are you logged in?)";
-                        callback(createResponse(messages));
+                        callback(createResponse(messages, baggage));
                         return;
                     }).success(function(data) {
                         if (!data.members || (data.members.names.length == 0 && data.members.roles.length == 0)) {
                             messages.vaultDbSec = "'vaultDbName' " + idDoc.vaultDbName + " is public";
-                            callback(createResponse(messages));
+                            callback(createResponse(messages, baggage));
                             return;
                         }
                         messages.vaultDbSec = null;
 
+                        baggage.vaultDbVault = { vaultDbName: idDoc.vaultDbName, vaultId: idDoc.vaultId };
                         var vaultDb = couchServer.getDB(idDoc.vaultDbName);
                         var vaultDoc = new vaultDb.docClass();
                         vaultDoc.load(idDoc.vaultId).error(function() {
                             messages.vaultDbVault = "No entry in " + idDoc.vaultDbName + " for current vault id";
-                            callback(createResponse(messages));
+                            callback(createResponse(messages, baggage));
                             return;
                         }).success(function() {
                             messages.vaultDbVault = null;
-                            callback(createResponse(messages));
+                            callback(createResponse(messages, baggage));
                             return;
                         });
                     });
@@ -100,8 +105,62 @@ angular.module("vault.services.installer", ["couchapp.service", "CornerCouch"])
             });
         };
 
-        this.fixProblems = function(callback) {
-            callback(createResponse({}));
-        }
+        this.fixIdDoc = function(vaultId, vaultDbName, callback) {
+            var currentDb = couchServer.getDB(CouchAppService.currentDb());
+            var idDoc = new currentDb.docClass({ _id: "id" });
+            idDoc.load().error(function() {
+                save();
+            }).success(function() {
+                save();
+            });
+
+            var save = function() {
+                idDoc.vaultId = vaultId;
+                idDoc.vaultDbName = vaultDbName;
+                idDoc.save().error(function() {
+                    callback(false);
+                }).success(function() {
+                    callback(true);
+                });
+            };
+        };
+
+        this.fixDbMembers = function(vaultDbName, callback) {
+            var url = "/" + vaultDbName + "/_security";
+            $http.get(url).error(function() {
+                callback(false);
+            }).success(function(data) {
+                if (!("members" in data)) {
+                    data.members = { names: [], roles:[]};
+                }
+                if (!("roles" in data.members)) {
+                    data.members.roles = [];
+                }
+                if (data.members.roles.length == 0) {
+                    data.members.roles.push("vaulters");
+                }
+                $http.put(url, data).error(function() {
+                    callback(false);
+                }).success(function() {
+                    callback(true);
+                });
+            });
+        };
+
+        this.fixVaultDbEntry = function(vaultDbName, vaultId, signature, callback) {
+            var vaultDB = couchServer.getDB(vaultDbName);
+            var vaultDoc = new vaultDB.docClass({
+                _id: vaultId,
+                type: "vault",
+                name: "New Vault",
+                dbs: [],
+                signature: signature
+            });
+            vaultDoc.save().error(function() {
+                callback(false);
+            }).success(function() {
+                callback(true);
+            });
+        };
     }
 ]);
